44a7f34d180756d52409059afc2931d6a138d463
Andreas Scherbaum <andreas@scherbaum.la>
2023-11-05 23:48:32 +0100

    Remove remnants of hardcoded table prefixes

--- s9y-to-hugo.py
+++ s9y-to-hugo.py
@@ -414,7 +414,7 @@ class DatabasePG:
 
 
     def s9y_config_entry(self, entry):
-        query = "SELECT value FROM serendipity_config where authorid = 0 and name = %s"
+        query = "SELECT value FROM {p}_config where authorid = 0 and name = %s".format(p = self.dbprefix)
         result = self.execute_one(query, [entry])
 
         return result[0]
@@ -705,7 +705,7 @@ class Migration:
 
         # $serendipity[‘useServerOffset’]: Boolean whether the timezone of the server and the authors differs
         # $serendipity[‘serverOffsetHours’]: How many hours timezone difference are between server and authors
-        q1 = "SELECT value FROM serendipity_config where authorid = 0 and name = 'useServerOffset'"
+        q1 = "SELECT value FROM {p}_config where authorid = 0 and name = 'useServerOffset'".format(p = self.dbprefix)
         r1 = self.db.execute_query(q1, [])
         if (r1[0]['value'] == 'true'):
             self.useServerOffset = True
@@ -714,7 +714,7 @@ class Migration:
         else:
             self.useServerOffset = False
 
-        q2 = "SELECT value FROM serendipity_config where authorid = 0 and name = 'serverOffsetHours'"
+        q2 = "SELECT value FROM {p}_config where authorid = 0 and name = 'serverOffsetHours'".format(p = self.dbprefix)
         r2 = self.db.execute_query(q2, [])
         self.useServerOffset = r2[0]['value']
 
